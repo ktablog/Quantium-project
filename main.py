a3f33b7285428fbd7352db9d09574d85c6db6ad0
kathy <kathy_aung@outlook.com>
2022-06-30 13:56:01 +0630

    modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import os
+import glob
+import pandas as pd
 
-path = ""
-
-for x in os.listdir():
-    if x.endswith(".txt"):
-        # Prints only text file present in My Folder
-        print(x)
+path = "D:\SoftwareDevelopment\_FORAGE\Quantium\quantium-project\quantium-starter-repo\data"
+csv_files = glob.glob(path + "/*.csv")
+df_list = (pd.read_csv(file) for file in csv_files)
+df   = pd.concat(df_list, ignore_index=True)
+print(df)
+df = df[df['product'].str.contains('pink')]
+print(df)
+df['price'] = df['price'].str.replace('$', '', regex=True)
+df['price'] = df['price'].astype(float)
+df['quantity'] = df['quantity'].astype(int)
+df['sales'] = df['price'] * df['quantity']
+print(df)
+df.drop('price', inplace=True, axis=1)
+df.drop('quantity', inplace=True, axis=1)
+df.drop('product', inplace=True, axis=1)
+print(df)
+df = df.reindex(columns=['sales', 'date', 'region'])
+print(df)
